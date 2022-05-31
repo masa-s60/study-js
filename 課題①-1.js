1a1d0fb134adc49bf20a42a0162623ef335ed0c0
masatoshi kusano <uppleate@cordiary.net>
2022-05-31 15:24:54 +0900

    修正完了

--- 課題①-1.js
+++ 課題①-1.js
@@ -1,13 +1,16 @@
+const FizzValue = 3;
+const BuzzValue = 5;
+
 for (let i = 1; i <= 99; i++) {
-  if((i % 3 === 0) && (i % 5 === 0)){
-    console.log('FizzBuzz' + i);
-    continue; 
+  if((i % FizzValue === 0) && (i % BuzzValue === 0)) {
+    console.log(`FizzBuzz${i}`);
+    continue;
   } else {
-    if(i % 3 === 0){
-      console.log('Fizz' + i); 
+    if(i % 3 === 0) {
+      console.log(`Fizz${i}`); 
     } else {
-      if(i % 5 === 0){
-        console.log('Buzz' + i); 
+      if(i % 5 === 0) {
+        console.log(`Buzz${i}`); 
       }
     }
   }
